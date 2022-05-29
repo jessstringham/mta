f3104c96e71bb92c0590d98624ba09536cbfe245
Jessica Stringham <jessstringham@users.noreply.github.com>
2022-05-29 15:17:38 -0400

    use blue instead

--- stop_config.py
+++ stop_config.py
@@ -9,7 +9,7 @@ class StopConfig:
     TIMING_GETTING_CLOSE = 2  # before this, turn red
     COLOR_HAVE_TIME = Color("G", "good time to leave")
     TIMING_HAVE_TIME = 5  # before this, turn green
-    COLOR_CAN_WAIT = Color("Y", "get ready")
+    COLOR_CAN_WAIT = Color("B", "get ready")
     TIMING_CAN_WAIT = 10  # after this, turn gray
     COLOR_TOO_FAR = Color("g", "too far")
     TIMING_TOO_FAR = 20  # after this, turn off
